f064d069c95892ffe32cc08401e639a98f8ac8a3
villeneve <villeneveoliveira@gmail.com>
2024-10-29 20:01:07 -0300

    Adição da arquitetura VGG16 novamente

---
+++ VGG16/config.py
@@ -0,0 +1,32 @@
+# some training parameters
+
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+import copy
+
+EPOCHS = 50
+BATCH_SIZE = 10
+NUM_CLASSES = 5
+DROPOUT = 0.5
+n = 1
+res = 600
+code = '0-1-2-3-4_gauss'
+image_size = (res,res,3)
+
+# trainy = np.load('../cut_trainy_random.npy')
+# trainy = np.array(to_categorical(trainy), dtype = 'uint8')
+
+# valy = np.load('../cut_valy_random.npy')
+# valy = np.array(to_categorical(valy), dtype = 'uint8')
+
+# valx = np.memmap('../cut_valx_COLOR_NORMALIZATION_NORMALIZED_random.dat',
+#                     dtype='float32',
+#                     mode='r',
+#                     shape=(valy.shape[0],224,224,3))
+
+#   #valx = preprocess_input(valx)
+
+# trainx = np.memmap('../cut_trainx_COLOR_NORMALIZATION_NORMALIZED_random.dat',
+#                     dtype='float32',
+#                     mode='r',
+#                     shape=(trainy.shape[0],224,224,3))
